52eff42fa51564b43df01d2d9c31fa27690e06a8
Kevin Sullivan <admin@sideapps.com>
2015-12-13 13:23:29 -0500

    Gear Data
    Finished scripts to parse XML dump into JSON

--- SplatScripts/gearImageParse.py
+++ SplatScripts/gearImageParse.py
@@ -1,13 +1,20 @@
+import pickle
 from shutil import copy
 from os import listdir
 
-src = "/Users/kevin/Downloads/wikiteam-master/splatoonwikiorg_w-20151212-wikidump/images"
-dest = "/Users/kevin/Documents/github/SplatPal/SplatPal/Images/gear/"
+src = '/Users/kevin/Downloads/wikiteam-master/splatoonwikiorg_w-20151212-wikidump/images'
+dest = '/Users/kevin/Documents/github/SplatPal/SplatPal/Images/gear/'
 images = [f for f in listdir(src)]
+resultText = []
 
 for f in images:
-	if f[-3:] == "png" and not "Beta" in f:
-		if "Geart Shoes" in f:
-			copy(src + "/" + f, dest + f[12:])
-		if "Geart Clothing" in f or "Geart Headgear" in f:
-			copy(src + "/" + f, dest + f[15:])
+	if f[-3:] == 'png' and not 'Beta' in f:
+		if 'Geart Shoes' in f:
+			resultText.append(f[12:][:-4])
+			copy(src + '/' + f, dest + 'gear' + f[12:].replace(' ', ''))
+		if 'Geart Clothing' in f or 'Geart Headgear' in f:
+			resultText.append(f[15:][:-4])
+			copy(src + '/' + f, dest + 'gear' + f[15:].replace(' ', ''))
+
+with open('gearNames.dat', 'wb') as f:
+	pickle.dump(resultText, f)
