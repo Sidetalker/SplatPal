10a58ef97541168a046bed7b808fcfe4129b9fe6
Kevin Sullivan <admin@sideapps.com>
2015-12-22 16:18:02 -0500

    Completed data parse

--- SplatScripts/weaponParse.py
+++ SplatScripts/weaponParse.py
@@ -6,8 +6,8 @@ import xml.etree.ElementTree as ET
 src = '/Users/kevin/Downloads/wikiteam-master/splatoonwikiorg_w-20151212-wikidump/splatoonwikiorg_w-20151212-current.xml'
 prp = '{http://www.mediawiki.org/xml/export-0.10/}'
 
-# with open('gearNames.dat', 'rb') as f:
-# 	gearNames = pickle.load(f)
+with open('weaponNames.dat', 'rb') as f:
+	weaponNames = pickle.load(f)
 
 root = ET.parse(src).getroot()
 data = []
@@ -15,44 +15,49 @@ data = []
 for child in root:
 	title = child.find(prp + 'title')
 	if title is not None:
-		print title.text 
+		if title.text in weaponNames:
+			curWeapon = title.text
+			weapon = {}
+			weapon['name'] = curWeapon
 
-		
-# 		if title.text in gearNames:
-# 			gear = {}
-# 			gear['category'] = 'Missing'
-# 			gear['brand'] = 'Missing'
-# 			gear['ability'] = 'Missing'
-# 			gear['rarity'] = 'Missing'
-# 			gear['cost'] = 'Missing'
+			gearFull = child.find(prp + 'revision').find(prp + 'text').text
+			reg = re.search('\{\{Infobox weapon([a]|[^a])*?(\n\}\}|\}\}\}\})', gearFull)
 
-# 			curGear = title.text
-# 			gearFull = child.find(prp + 'revision').find(prp + 'text').text
-# 			reg = re.search('\{\{Infobox gear([a]|[^a])*?\}\}', gearFull)
-
-# 			gear['name'] = curGear
-
-# 			if reg is not None:
-# 				gearReg = reg.group()
+			if reg is not None:
+				gearReg = reg.group()
 			
-# 				for line in gearReg.splitlines():
-# 					if line[:9] == '|category':
-# 						gear['category'] = line.split("= ", 1)[1]
-# 					if line[:6] == '|brand':
-# 						gear['brand'] = line.split("= ", 1)[1]
-# 					if line[:8] == '|ability':
-# 						gear['ability'] = line.split("= ", 1)[1]
-# 					if line[:7] == '|rarity':
-# 						gear['rarity'] = line.split("= ", 1)[1]
-# 					if line[:5] == '|cost':
-# 						gear['cost'] = line.split("= ", 1)[1]
-# 			else:
-# 				print 'Failed to parse data for ' + curGear
+				for line in gearReg.splitlines():
+					if 'cost' in line:
+						weapon['cost'] = line.split("= ", 1)[1]
+						if 'explain' in weapon['cost']:
+							weapon['cost'] = weapon['cost'].replace('{', '').replace('}', '').split("|", 2)[2]
+					if 'base' in line:
+						weapon['baseDmg'] = line.split("= ", 1)[1]
+						if 'explain' in weapon['baseDmg']:
+							weapon['baseDmg'] = weapon['baseDmg'].replace('{', '').replace('}', '').split("|", 2)[2]
+					if 'level' in line:
+						weapon['level'] = line.split("= ", 1)[1].split(" ", 1)[0]
+					if 'range' in line:
+						weapon['range'] = line.split("= ", 1)[1].split("|", 2)[1]
+					if 'attack' in line:
+						weapon['attack'] = line.split("= ", 1)[1].split("|", 2)[1]
+					if 'fire' in line:
+						weapon['fire'] = line.split("= ", 1)[1].split("|", 2)[1]
+					if 'sub' in line:
+						weapon['sub'] = line.split("= ", 1)[1].replace('[', '').replace(']', '')
+					if 'nocharge' in line:
+						weapon['nocharge'] = line.split("= ", 1)[1]
+					if 'fullcharge' in line:
+						weapon['fullcharge'] = line.split("= ", 1)[1]
+					if 'special' in line:
+						weapon['special'] = line.split("= ", 1)[1].replace('[', '').replace(']', '')
+			else:
+				print 'Failed to parse data for ' + curWeapon
 
-# 			data.append(gear)
+			data.append(weapon)
 
-# allGearData = {}
-# allGearData["gear"] = data
+allWeaponData = {}
+allWeaponData["weapons"] = data
 
-# with open('gearData.json', 'w') as f:
-#     json.dump(allGearData, f)
+with open('weaponData.json', 'w') as f:
+    json.dump(allWeaponData, f)
