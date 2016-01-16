c8836ecf685b2f762f8c9c5821eea2a75d6a5c29
Kevin Sullivan <admin@sideapps.com>
2016-01-16 09:40:06 -0500

    Today View Splatfest Designed

--- SplatWidget/TodayViewController.swift
+++ SplatWidget/TodayViewController.swift
@@ -40,16 +40,20 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     @IBOutlet weak var dividerCenter: UIView!
     @IBOutlet weak var divider1and2: UIView!
     @IBOutlet weak var divider2and3: UIView!
+    
+    @IBOutlet weak var splatfestView: UIView!
         
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        preferredContentSize = CGSizeMake(preferredContentSize.width, 190)
+        self.preferredContentSize = CGSizeMake(self.preferredContentSize.width, 1)
         
         // Hide all views before initial load
         for view in self.view.subviews {
             view.alpha = 0.0
         }
+        
+        splatfestView.backgroundColor = UIColor.clearColor()
     }
     
     override func didReceiveMemoryWarning() {
@@ -88,14 +92,20 @@ class TodayViewController: UIViewController, NCWidgetProviding {
             }
             else {
                 if data["splatfest"].boolValue {
-                    NSLog("Splatfest found, performing segue")
-                    self.performSegueWithIdentifier("segueSplatfest", sender: self)
-                    self.preferredContentSize = CGSizeMake(self.preferredContentSize.width, 86)
+                    NSLog("Splatfest found")
+                    
+                    self.preferredContentSize = CGSizeMake(self.preferredContentSize.width, 82)
+                    
+                    
+                    
+                    self.splatfestView.alpha = 1.0
                 } else {
+                    self.preferredContentSize = CGSizeMake(self.preferredContentSize.width, 190)
+                    
                     // Unhide all views if they're hidden
                     if self.dividerCenter.alpha == 0.0 {
                         for view in self.view.subviews {
-                            view.alpha = 1.0
+                            if view != self.splatfestView { view.alpha = 1.0 }
                         }
                     }
                     
@@ -108,55 +118,6 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "segueSplatfest" {
             splatController = segue.destinationViewController as? SplatfestViewController
-        }
-    }
-}
-
-class SplatfestViewController: UIViewController, NCWidgetProviding {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        
-//        setNeedsFocusUpdate()
-//        setNeedsStatusBarAppearanceUpdate()
-        
-    }
-    
-    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)) {
-        // Perform any setup necessary in order to update the view.
-        
-        // If an error is encountered, use NCUpdateResult.Failed
-        // If there's no update required, use NCUpdateResult.NoData
-        // If there's an update, use NCUpdateResult.NewData
-        
-        completionHandler(NCUpdateResult.NewData)
-        
-        // Configure NNID cookies
-        let nnid = NNID.sharedInstance
-        
-        if !nnid.saveLogin {
-            nnid.clearCookies()
-        }
-        else {
-            if let cookie = nnid.cookieObj {
-                Alamofire.Manager.sharedInstance.session.configuration.HTTPCookieStorage?.setCookie(NSHTTPCookie(properties: cookie)!)
-            } else { loginNNID { error in } }
-        }
-        
-        loadMaps { data in
-            if data["errorCode"].int != nil {
-                completionHandler(NCUpdateResult.Failed)
-            }
-            else {
-                if !data["splatfest"].boolValue {
-                    NSLog("No Splatfest - dismissing self")
-                    self.dismissViewControllerAnimated(false, completion: nil)
-                }
-            }
         }
     }
 }
